9219d93a8fdf299b0325f79cfa094c0d4c8a78f8
Dessalines <tyhou13@gmx.com>
2020-11-19 15:03:52 -0600

    Making a simple lemmy front end.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lemmy-front-end-example
+
+## Requirements
+
+- yarn
+- Possibly a CORS plugin like [Allow CORS](https://addons.mozilla.org/en-US/firefox/addon/access-control-allow-origin/)
+
+## Install
+
+```
+yarn
+yarn start
+```
+
+And goto http://localhost:4444
+
---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import { render, Component } from 'inferno'
+import { LemmyHttp, SiteResponse } from 'lemmy-js-client';
+
+const httpUrl = `http://${window.location.hostname}:8536/api/v1`;
+
+const container = document.getElementById('app')
+
+interface MyProps {}
+
+interface MyState {
+  site: SiteResponse,
+}
+
+class MyComponent extends Component<MyProps, MyState> {
+  client: LemmyHttp;
+  constructor(props: any, context: any) {
+    super(props, context);
+    this.client = new LemmyHttp(httpUrl);
+    this.state = {
+      site: null,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({site: await this.client.getSite({})});
+  }
+
+  public render() {
+    return (
+      <div>
+        {this.state.site && (
+          <>
+            <h1>{`Hey there from Lemmy: ${this.state.site.site.name}`}</h1>
+            <h2>Site data:</h2>
+            <pre>{JSON.stringify(this.state.site, null, 2)}</pre>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+render(<MyComponent />, container)
